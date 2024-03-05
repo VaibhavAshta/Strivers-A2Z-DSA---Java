02243503667acd74f74c43e398312162f212335c
VaibhavAshta <88577836+VaibhavAshta@users.noreply.github.com>
2024-03-06 00:52:09 +0530

    Create two-sum.java

---
+++ Arrays/Medium/two-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[]{nums[left], nums[right]};
+            } else if (sum < target) {
+                left++;
+            }else{  
+                right--;
+            }
+        }
+        return null;
+    }
+}
+//https://leetcode.com/problems/two-sum/description/
